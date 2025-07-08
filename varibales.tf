dfc29030f57c47d7e95ef9feea89b21d31121883
barick <barick3018@gmail.com>
2025-07-09 04:41:48 +0530

    Initial Comimt with basic files

---
+++ varibales.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID to use"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 Instance Type"
+  type        = string
+  default     = "t2.micro"
+}
