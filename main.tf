501c34b52e39b1ea9f06eadbea3c02ede9ae3d72
barick <barick3018@gmail.com>
2025-07-14 12:11:25 +0530

    adding encryption to root volume to test snyk

--- main.tf
+++ main.tf
@@ -5,6 +5,9 @@ provider "aws" {
 resource "aws_instance" "my_ec2" {
   ami           = var.ami_id
   instance_type = var.instance_type
+  root_block_device {
+    encrypted = true
+  }
   tags = {
     Name = "MyEC2Instance"
     Cost_Center = "Prod"
